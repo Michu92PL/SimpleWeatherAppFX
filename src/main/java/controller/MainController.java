bc2268a764ba01ee5961f4ba7825f44eea04c6e2
Michu <mich1992@wp.pl>
2018-09-13 20:06:41 +0200

    add maven dependency for jar, minor fixes

--- src/main/java/controller/MainController.java
+++ src/main/java/controller/MainController.java
@@ -1,12 +1,14 @@
 package controller;
 
 import javafx.fxml.FXML;
+
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.image.ImageViewBuilder;
+import javafx.scene.layout.Pane;
 import model.Weather;
 import model.WeatherService;
 
@@ -36,6 +38,7 @@ public class MainController {
 
     @FXML
     private Label nameLabel;
+
 
     @FXML
     public void showWeather() {
--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -15,6 +15,7 @@ public class Main extends Application {
     }
 
     public void start(Stage primaryStage) throws Exception {
+        System.out.println("javafx.runtime.version: " + System.getProperties().get("javafx.runtime.version"));
 
         FXMLLoader loader = new FXMLLoader(this.getClass().getResource("/fxml/MainView.fxml"));
         Pane pane = loader.load();
@@ -22,9 +23,15 @@ public class Main extends Application {
         MainController controller = loader.getController();
         controller.setWeatherService(new WeatherService());
 
-        Scene scene = new Scene(pane);
+        final Scene scene = new Scene(pane,313,426);
+        //Scene scene = new Scene(pane);
         primaryStage.setScene(scene);
+        primaryStage.sizeToScene();
         primaryStage.setTitle("S.W. App");
+//        primaryStage.setMaxHeight(450);
+//        primaryStage.setMinHeight(450);
+//        primaryStage.setMaxWidth(321);
+//        primaryStage.setMinWidth(321);
         primaryStage.getIcons().add(new Image(this.getClass().getResourceAsStream("/icon.png")));
         primaryStage.setResizable(false);
         primaryStage.show();
